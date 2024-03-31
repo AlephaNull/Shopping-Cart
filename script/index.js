89a6c25b3286617bf72febc9a34fe1d99f8748d9
Alepha Null <HashTag>
2024-04-01 01:22:06 +0530

    Changed CSS; Added more JS functionality

--- script/index.js
+++ script/index.js
@@ -1,7 +1,50 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+
 const addBtn = document.getElementById("add-button")
 const inputField = document.getElementById("input-field")
+const shoppingListEl = document.getElementById("shopping-list")
 
-let inputValue = inputField.value
+const appSettings = {
+  databaseURL: "https://shopping-cart-ebc11-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDb = ref(database, "shoppingList")
+
 addBtn.addEventListener("click", function() {
-  console.log(inputValue)
+  let inputValue = inputField.value
+  push(shoppingListInDb, inputValue)
+  clearInput()
+  // renderList(inputValue)
 })
+
+onValue(shoppingListInDb, function(snapshot) {
+  let itemsArr = Object.entries(snapshot.val())
+  clearShoppingListEl()
+  for (const item of itemsArr) {
+    let currentItem = item
+    let currentItemID = itemsArr[0]
+    let currentItemValue = currentItem[1]
+    renderList(currentItemValue)
+  }
+})
+
+function clearShoppingListEl() {
+  shoppingListEl.innerHTML = ""
+}
+function clearInput() {
+  inputField.value = ""
+}
+
+function renderList(item) {
+  let itemID = item[0]
+  let itemValue = item[1]
+
+  let newEl = document.createElement("li")
+  newEl.textContent = itemValue
+  shoppingListEl.append(newEl)
+
+}
